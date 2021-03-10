532f5623af7f20593767c8e2f29e5394f26a137f
rsaha <rsaha@rsglab.com>
2021-03-10 15:00:33 +0600

    testrunner file implemented

--- src/test/java/testRunner/TestRunner.java
+++ src/test/java/testRunner/TestRunner.java
@@ -1,4 +1,16 @@
 package testRunner;
 
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = {"src/test/java/features"},
+        tags = {""},
+        glue = "steps",
+        monochrome = true,
+        plugin = {"pretty", "html:test-output"}
+)
 public class TestRunner {
 }
