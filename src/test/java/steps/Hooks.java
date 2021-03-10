2f14d47e2d16211aaa8131b1347ee4680e06c02b
rsaha <rsaha@rsglab.com>
2021-03-10 15:07:24 +0600

    Hooks , app
    Hooks implemented and app added in app folder

--- src/test/java/steps/Hooks.java
+++ src/test/java/steps/Hooks.java
@@ -1,4 +1,16 @@
 package steps;
 
-public class Hooks {
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+
+public class Hooks extends TestSetup{
+    @Before
+    public void setUpDriver() {
+        startDriver();
+    }
+
+    @After
+    public void tearDown() {
+        stopDriver();
+    }
 }
--- src/test/java/steps/TestSetup.java
+++ src/test/java/steps/TestSetup.java
@@ -21,7 +21,7 @@ public class TestSetup {
             caps.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, "1200");
             caps.setCapability(AndroidMobileCapabilityType.AUTO_GRANT_PERMISSIONS, "true");
             caps.setCapability(MobileCapabilityType.PLATFORM_VERSION, "9");
-            caps.setCapability(MobileCapabilityType.APP, System.getProperty("user.dir") + "/app/sorolipi.apk");
+            caps.setCapability(MobileCapabilityType.APP, System.getProperty("user.dir") + "/App/PreciseUnitConversion.apk");
             caps.setCapability(MobileCapabilityType.FULL_RESET, "true");
             caps.setCapability(MobileCapabilityType.NO_RESET, "false");
             Logger.getLogger("org.openqa.selenium").setLevel(Level.OFF);
