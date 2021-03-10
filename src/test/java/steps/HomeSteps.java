54fcca99aacbd96b248b331cc54ac88f9c32a600
rsaha <rsaha@rsglab.com>
2021-03-10 15:24:07 +0600

    teststeps added

--- src/test/java/steps/HomeSteps.java
+++ src/test/java/steps/HomeSteps.java
@@ -1,4 +1,17 @@
 package steps;
 
-public class HomeSteps {
+import cucumber.api.java.en.*;
+
+public class HomeSteps extends TestSetup{
+    @Given("^I open the application$")
+    public void i_open_the_application(){
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+
+    @Then("^I should see Home Page$")
+    public void i_should_see_Home_Page(){
+        // Write code here that turns the phrase above into concrete actions
+
+    }
 }
